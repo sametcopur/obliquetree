2a3726a5e4bc3e8799302af95e69b3b249409178
samet çopur <sametcopur@yahoo.com>
2026-03-15 01:50:15 +0300

    paralel test

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import os
 import numpy as np
 from setuptools.extension import Extension
 import sys
+
+include_dirs_extra = []
+library_dirs = []
+extra_compile_args = []
+extra_link_args = []
 
 if sys.platform in ("win32", "linux", "darwin"):  # All platforms
     if sys.platform == "win32":
@@ -27,8 +33,26 @@ if sys.platform in ("win32", "linux", "darwin"):  # All platforms
         ]
         extra_link_args = []
 
+        if sys.platform == "linux":
+            extra_compile_args.append("-fopenmp")
+            extra_link_args.append("-fopenmp")
+        elif sys.platform == "darwin":
+            libomp_prefix = os.environ.get("LIBOMP_PREFIX", "/opt/homebrew/opt/libomp")
+            if os.path.isdir(libomp_prefix):
+                include_dirs_extra = [os.path.join(libomp_prefix, "include")]
+                library_dirs = [os.path.join(libomp_prefix, "lib")]
+                extra_compile_args.extend(["-Xpreprocessor", "-fopenmp"])
+                extra_link_args.extend([
+                    "-L" + library_dirs[0],
+                    "-lomp",
+                    "-Wl,-rpath," + library_dirs[0],
+                ])
+            else:
+                include_dirs_extra = []
+                library_dirs = []
+
 define_macros = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-include_dirs = [np.get_include()]
+include_dirs = [np.get_include(), *include_dirs_extra]
 
 
 extensions = [
@@ -36,48 +60,60 @@ extensions = [
         "obliquetree.src.tree",
         ["obliquetree/src/tree.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
     Extension(
         "obliquetree.src.oblique",
         ["obliquetree/src/oblique.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
     Extension(
         "obliquetree.src.base",
         ["obliquetree/src/base.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
     Extension(
         "obliquetree.src.utils",
         ["obliquetree/src/utils.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
     Extension(
         "obliquetree.src.metric",
         ["obliquetree/src/metric.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
     Extension(
         "obliquetree.src.ccp",
         ["obliquetree/src/ccp.pyx"],
         include_dirs=include_dirs,
+        library_dirs=library_dirs,
         define_macros=define_macros,
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
 ]
